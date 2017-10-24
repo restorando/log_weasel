ee2593b04ec515b832c86aa2ceae31c5d3eac82c
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-24 16:16:30 -0300

    Fix log weasel specs
    - Remove key spec
    - Add new specs

--- spec/log_weasel/log_weasel_spec.rb
+++ spec/log_weasel/log_weasel_spec.rb
@@ -1,15 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require 'resque'
 require 'log_weasel'
 
 describe LogWeasel do
 
   describe ".configure" do
-    it "stores key" do
-      LogWeasel.configure do |config|
-        config.key = "KEY"
+    context "when called with no args" do
+      it "maintains default config" do
+        expect { LogWeasel.configure {} }.to_not change(LogWeasel, :config)
       end
-      LogWeasel.config.key.should == "KEY"
+    end
+
+    it "is disabled by default" do
+      expect(LogWeasel.config.enabled).to be false
+    end
+
+    it "stores header name" do
+      LogWeasel.configure do |config|
+        config.header_name = 'X_TEST'
+      end
+      expect(LogWeasel.config.header_name).to eq 'X_TEST'
     end
   end
 
