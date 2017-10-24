42d0e50b00deaa44eac9971dda0f4461d00ade63
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-24 15:36:40 -0300

    Refactor middleware
    - Use new config
    - Stop using key
    - Don't attach transaction ID if missing

--- lib/log_weasel/middleware.rb
+++ lib/log_weasel/middleware.rb
@@ -1,31 +1,23 @@
 class LogWeasel::Middleware
-  KEY_HEADER = 'X_LOGWEASEL_KEY'
-  TRANSACTION_ID_HEADER = 'X_LOGWEASEL_TRANSACTION_ID'
 
-  def initialize(app, options = {})
+  def initialize(app, _options = {})
     @app = app
-    @key = LogWeasel.config.key ? "#{LogWeasel.config.key}-WEB" : "WEB"
+    @config = LogWeasel.config
   end
 
-  # TODO: Is this right? Do we even want to do this? I'm not sure
-  # What I'm doing is getting transaction ID from header (or creating it if nonexistent) and attaching it as
-  # header for the next request. Thing is, one of the two needs to happen:
-  # a) The client stores the header and sends it in its next request
-  # b) We add the header before calling `@app.call`, so that if the call is going elsewhere (ie. Redo-API)
-  #    they will receive the header
-
+  # Get transaction ID from header, or create if missing and configured to do so.  If present (or created),
+  # attach it as header so the other app uses it as well.
   def call(env)
-    key = env.fetch("HTTP_#{KEY_HEADER}", @key)
     # Get or create transaction ID
-    transaction_id = env.fetch("HTTP_#{TRANSACTION_ID_HEADER}", nil)
-    if transaction_id.nil?
-      transaction_id = LogWeasel::Transaction.create(key)
-    else
-      LogWeasel::Transaction.id = transaction_id
+    transaction_id = env.fetch("HTTP_#{@config.header_name}", nil)
+    if transaction_id.nil? && @config.generate_id_if_missing
+      transaction_id = LogWeasel::Transaction.create
     end
+    LogWeasel::Transaction.id = transaction_id unless transaction_id.nil?
+
     status, headers, body = @app.call(env)
-    # Attach transaction ID to header
-    headers[TRANSACTION_ID_HEADER] = transaction_id
+    # Attach transaction ID to header if necessary
+    headers[@config.header_name] = transaction_id unless transaction_id.nil?
 
     return [status, headers, body]
   ensure
