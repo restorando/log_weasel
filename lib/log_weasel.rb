53552995a643632a0d8c5a38b8fe366f194f4326
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-30 12:24:39 -0300

    Missing Rubocop fixes

--- lib/log_weasel.rb
+++ lib/log_weasel.rb
@@ -1,14 +1,14 @@
 require "log_weasel/transaction"
 require "log_weasel/middleware"
 require "log_weasel/railtie" if defined? ::Rails::Railtie
-require 'hashie'
+require "hashie"
 
 module LogWeasel
   def self.config
     @@config ||= Hashie::Mash.new(
       # TODO: Document
       enabled: false,
-      header_name: 'X_TRANSACTION_ID',
+      header_name: "X_TRANSACTION_ID",
       id_generator: lambda { SecureRandom.hex(10) },
       generate_id_if_missing: true
     )
